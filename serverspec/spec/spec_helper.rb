c9bafdb87a58e58637c9069a9222e9bef6b3b920
EC2 Default User <ec2-user@ip-10-0-6-109.ap-northeast-1.compute.internal>
2024-10-06 14:08:18 +0000

    test

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -2,10 +2,10 @@ require 'serverspec'
 
 set :backend, :ssh
 
-set :host, 'ec2-57-180-213-93.ap-northeast-1.compute.amazonaws.com'
+set :host, 'ec2-54-150-159-229.ap-northeast-1.compute.amazonaws.com'
 set :ssh_options, {
   user: 'ec2-user',
-  keys: ['/home/taemimizukura/.ssh/RaiseTech-Key.pem'],
+  keys: [ENV['SSH_KEY_PATH']],
   forward_agent: true,
   auth_methods: %w(publickey)
 }
